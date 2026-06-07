3bfaf88e52686aa3a4fb15233b74d76ee17afd66
rajabalakrishnan-cmyk <raja.balakrishnan@cloudly.io>
2026-06-07 18:40:10 +0530

    Fix job report

--- job_search.py
+++ job_search.py
@@ -6,21 +6,21 @@ TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
 companies = [
-("HUL", "https://www.hul.co.in/careers/"),
-("ITC", "https://www.itcportal.com/careers/index.aspx"),
-("Dabur", "https://www.dabur.com/careers"),
-("Marico", "https://marico.com/india/careers"),
-("Godrej", "https://www.godrejcareers.com/"),
-("Britannia", "https://www.britannia.co.in/careers"),
-("Nestle", "https://www.nestle.in/jobs"),
-("P&G", "https://www.pgcareers.com/in/en"),
-("HCCB", "https://www.hccb.in/career"),
-("Amrutanjan", "https://www.amrutanjan.com/careers/"),
-("Dr Reddy's", "https://careers.drreddys.com/"),
-("Titan", "https://www.titancompany.in/careers"),
-("CavinKare", "https://www.cavinkare.com/careers/"),
-("Michael Page", "https://www.michaelpage.co.in/jobs/fmcg"),
-("CIEL HR", "https://www.cielhr.com/jobs/fmcg")
+    ("HUL", "https://www.hul.co.in/careers/"),
+    ("ITC", "https://www.itcportal.com/careers/index.aspx"),
+    ("Dabur", "https://www.dabur.com/careers"),
+    ("Marico", "https://marico.com/india/careers"),
+    ("Godrej", "https://www.godrejcareers.com/"),
+    ("Britannia", "https://www.britannia.co.in/careers"),
+    ("Nestle", "https://www.nestle.in/jobs"),
+    ("P&G", "https://www.pgcareers.com/in/en"),
+    ("HCCB", "https://www.hccb.in/career"),
+    ("Amrutanjan", "https://www.amrutanjan.com/careers/"),
+    ("Dr Reddy's", "https://careers.drreddys.com/"),
+    ("Titan", "https://www.titancompany.in/careers"),
+    ("CavinKare", "https://www.cavinkare.com/careers/"),
+    ("Michael Page", "https://www.michaelpage.co.in/jobs/fmcg"),
+    ("CIEL HR", "https://www.cielhr.com/jobs/fmcg")
 ]
 
 message = f"""
@@ -51,7 +51,7 @@ Career Sites Checked:
 """
 
 for company, url in companies:
-message += f"\n• {company}"
+    message += f"\n• {company}"
 
 message += """
 
@@ -66,11 +66,11 @@ Actual job extraction from company portals.
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 response = requests.post(
-url,
-json={
-"chat_id": CHAT_ID,
-"text": message
-}
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
 )
 
 print(response.status_code)
